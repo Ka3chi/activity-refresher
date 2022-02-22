337415d430f1a4c428a47607eb6c12377512ad3e
Ka3chi <aldrinalmoradie69@gmail.com>
2022-02-22 20:59:41 +0800

    Relles, Almoradie, Ducay, Pastorpide

--- School.ts
+++ School.ts
@@ -37,10 +37,16 @@ const studentList = new StudentList([
     student3
 ]);
 
-console.log(studentList.getStudents());
+// console.log(studentList.getStudents());
 // console.log("*************");
 // console.log(studentList.updateStudentName("2015-105869", "Michael Jordan"));
 // console.log("*************");
-console.log(studentList.updateAge("2015-105869", 25));
+/*console.log(studentList.updateAge("2015-105869", 25));
+console.log(studentList.updateCourse("2015-105869", "BSBA"));
+console.log(studentList.updateCollege("2015-105869", "BAKS"));
+console.log(studentList.updateAddress("2015-105869", "Tondo"));
 // console.log(studentList.checkStudentId("2015-105870"));
+console.log(studentList.numberOfStudent());
+console.log(deletestudent.numberOfStudent());*/
+console.log(studentList.deleteStudent(0,1));
 console.log(studentList.getStudents());
--- StudentList.ts
+++ StudentList.ts
@@ -55,17 +55,43 @@ export class StudentList {
     }
 
     updateCourse(id: string, courseName: string) {
-
-    }
+            let student = this.students.find(element => element.studentId === id);
+            if (student) {
+                  student.course = courseName;
+                  return student;
+              }
+              return "student does not exist"
+          }
 
     updateCollege(id: string, collegeName: string) {
-
+        let student = this.students.find(element => element.studentId === id);
+        if (student) {
+              student.college = collegeName;
+              return student;
+          }
+          return "student does not exist"
     }
 
-    updateAddress(id: string, address: string) {
-
+    updateAddress(id: string, Address: string) {
+        let student = this.students.find(element => element.studentId === id);
+        if (student) {
+              student.address = Address;
+              return student;
+          }
+          return "student does not exist"
     }
 
     // create method and return number of students
+    numberOfStudent() {
+        return this.students.length
+    }
     // delete one student and return list of students, use splice method (index, 1)
+    deleteStudent(index:number, item:number){
+        
+        if(item == 1) {
+            this.students.splice (index, item)
+            return "Student Deleted"
+        }
+        return "One data remove"
+    }
 }
